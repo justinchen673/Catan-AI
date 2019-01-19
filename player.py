caf649c69cb1447ae8b13e2d5b61f1d8af9120fe
justinchen673 <justinchenchen673@gmail.com>
2019-01-19 18:18:31 -0500

    Change representation to dictionary

--- player.py
+++ player.py
@@ -5,11 +5,13 @@ class Player:
 
     def __init__(self, name):
         self.name = name
-        self.wheatCount = 0
-        self.sheepCount = 0
-        self.brickCount = 0
-        self.oreCount = 0
-        self.woodCount = 0
+        self.resourceDict = {
+            "wheat": 0,
+            "sheep": 0,
+            "brick": 0,
+            "ore": 0,
+            "wood": 0
+        }
         self.totalResources = 0
         self.devCards = []
         self.settlementList = []
