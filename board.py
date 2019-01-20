3a646b3acf440efee4ff5126ac0f05ddb7a05db3
justinchen673 <justinchenchen673@gmail.com>
2019-01-19 21:16:47 -0500

    Added relation matrices and made canplacesettlement

--- board.py
+++ board.py
@@ -39,13 +39,17 @@ class Board:
 
     # Pass in the list of vertices and hexes.
     def __init__(self, vertices, hexes):
+        # List of vertices
         self.vertices = vertices
+
+        # List of hexes
         self.hexes = hexes
-        self.roads = []
-        self.roadMatrix = []
-        for i in range(0, 54):
-            self.roadMatrix.append([])
-        '''
+
+        # Roads is a dictionary: Key is a tuple of the vertices, value is name
+        self.roads = {
+        }
+
+        # A matrix that tells what vertices each hex is linked to
         self.hexRelationMatrix = [
             [0, 3, 4, 7, 8, 12],
             [1, 4, 5, 8, 9, 13],
@@ -67,7 +71,91 @@ class Board:
             [40, 44, 45, 48, 49, 52],
             [41, 45, 46, 49, 50, 53]
         ]
-        '''
+
+        # A matrix that tells what vertices each vertex is linked to
+        self.vertexRelationMatrix = [
+            [3, 4],
+            [4, 5],
+            [5, 6],
+            [0, 7],
+            [0, 1, 8],
+            [1, 2, 9],
+            [2, 10],
+            [3, 11, 12],
+            [4, 12, 13],
+            [5, 13, 14],
+            [6, 14, 15],
+            [7, 16],
+            [7, 8, 17],
+            [8, 9, 18],
+            [9, 10, 19],
+            [10, 20],
+            [11, 21, 22],
+            [12, 22, 23],
+            [13, 23, 24],
+            [14, 24, 25],
+            [15, 25, 26],
+            [16, 27],
+            [16, 17, 28],
+            [17, 18, 29],
+            [18, 19, 30],
+            [19, 20, 31],
+            [20, 32],
+            [21, 33],
+            [22, 33, 34],
+            [23, 34, 35],
+            [24, 35, 36],
+            [25, 36, 37],
+            [26, 37],
+            [27, 28, 38],
+            [28, 29, 39],
+            [29, 30, 40],
+            [30, 31, 41],
+            [31, 32, 42],
+            [33, 43],
+            [34, 43, 44],
+            [35, 44, 45],
+            [36, 45, 46],
+            [37, 46],
+            [38, 39, 47],
+            [39, 40, 48],
+            [40, 41, 49],
+            [41, 42, 50],
+            [43, 51],
+            [44, 51, 52],
+            [45, 52, 53],
+            [46, 55],
+            [47, 48],
+            [48, 49],
+            [49, 50]
+        ]
+
+
+    def canPlaceSettlement(self, vertex, playerName, firstPlacement):
+        # Out of bounds vertex
+        if (vertex < 0 or vertex > 53):
+            return False
+
+        # Something already there
+        if not self.vertices[vertex].empty:
+            return False
+
+        # Something at an adjacent vertex
+        for i in self.vertexRelationMatrix[vertex]:
+            if not self.vertices[i].empty:
+                return False
+
+        # Checks if it's connected to a road if it isn't the first placement
+        if not firstPlacement:
+            for i in self.vertexRelationMatrix[vertex]:
+                if (vertex, i) in self.roads and self.roads[(vertex, i)] == playerName:
+                    return True
+                if (i, vertex) in self.roads and self.roads[(i, vertex)] == playerName:
+                    return True
+            return False
+
+        return True
+
 
     def formatHex(self,resource):
         extra_space = 0             # Counts extra space if word has an odd length.
@@ -83,7 +171,7 @@ class Board:
         resource_list = []
         number_list = []
         for i in self.hexes:
-            temp_str = self.formatHex(i.resourceType) 
+            temp_str = self.formatHex(i.resourceType)
             resource_list.append(temp_str)              # Resource list contains all formatted strings of resourceTypes.
             temp_str2 = self.formatHex(i.number)
             number_list.append(temp_str2)               # Number list contains all formatted strings of numbers.
