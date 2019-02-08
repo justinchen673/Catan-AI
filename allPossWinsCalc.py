bce1112ee7bf65886dd03dba4fb007bfb6b439a9
ishitapadhiar <padhii@rpi.edu>
2019-02-08 15:40:45 -0500

    Updated documentation and commenting

--- allPossWinsCalc.py
+++ allPossWinsCalc.py
@@ -2,14 +2,28 @@
 from winType import *
 
 '''
-[devVic, army, road, settlements, cities]
+win combo =[devVic, army, road, settlements, cities]
+'''
+
+'''
+This function find all the possible combinations of points for the game,
+however it does not include everything, specifically,HOW the roads are obtained
+	- ie, roadbuilding vs simple buying
+
+It returns a 2-d list of size 143 
+Each item inside the big list is the combination of points that lead to victory
+
 '''
 def findPossibleWins():
-
+	#all the possibilities of victory points (from dev cards) that may be obtained
 	devVic = [0,1,2,3,4,5]
+	#you either have largest army or not
 	largeArmy = [0,2]
+	#you either have longest road or not
 	longRoad = [0,2]
+	#max number of settlements
 	setts = [0,1,2,3,4,5]
+	#max vic points from cities
 	cits = [0,2,4,6,8]
 	winCombo = []
 
@@ -20,14 +34,22 @@ def findPossibleWins():
 					for m in cits:
 						aWin = [i,j,k,l,m]
 						vicPts = sum(aWin)
+						#not possible to have more than 12, since by then you would 
+						#have already won and the game would be over
 						if vicPts < 10 or vicPts > 12:
 							continue
+						#if 12 vic points,you have to have longest road and a settlement
+						#for more info look at the first webite in aiPrototype.txt
 						if vicPts == 12 and (k == 0 or l == 0 ):
 							continue
+						#if 11 vic points you have to get longest road or largest army
+						#for more info look at the first webite in aiPrototype.txt
 						if vicPts == 11 and (j == 0 and k == 0):
 							continue
+						#if you have no settlement you must have atleast 2 cities
 						if l==0 and m < 4:
 							continue
+						#if you have only 1 settlement you must have at least 1 ciy
 						if l==1 and m <2:
 							continue
 						winCombo.append(aWin)
@@ -35,13 +57,24 @@ def findPossibleWins():
 
 
 '''
+This function finds the minimum number of roads required to build that number of 
+settlements and cities
 
+The minroad changes depending on whether or not there is a longest road
 '''
 def findMinRoads(strat):
 	minRoads = 0
+	#you have to divideby 2 since the cities are stored as the number of victory points
+	#they are worth
 	numCits = int(strat[4]/2)
+
+	#no more than 7 settlements + cities in any of the solutions,
+	#since max 5 settlements + 2 cities = 9 vic points, after that must 
+	#convert a settlement to a city, resulting in 10
 	if strat[3] + numCits> 7:
 		print(strat, "HUH?????????")
+
+	#min road required without longest road
 	if strat[2] == 0:
 		if strat[3] + numCits == 2:
 			minRoads = 2
@@ -56,6 +89,7 @@ def findMinRoads(strat):
 		if strat[3] + numCits == 7:
 			minRoads = 8
 
+	#min roads required with longest road
 	if strat[2] == 2:
 		if strat[3] + numCits == 2:
 			minRoads = 5
@@ -73,7 +107,8 @@ def findMinRoads(strat):
 	return minRoads
 
 '''
-
+creates an object of the Win class and then returns it
+assgins in a number as well as all other relevant information that defines it
 '''
 def createWin(strategy, minRds, rBuilding, counter):
 	w = Win()
@@ -85,31 +120,38 @@ def createWin(strategy, minRds, rBuilding, counter):
 
 
 '''
+create all the possible basic win type, this time including the different costs incurred 
+by whether or not a road building card is used
 
+after accounting for this there are 348 different types of wins
 '''	
 def createWinList():
 	pointCombinations = findPossibleWins()
 	minRds = 0
 	counter = 0
 	wins = []
+
 	for strategy in pointCombinations:
 		minRds = findMinRoads(strategy)
+		#for all strategies, first include a win with 0 road building cards
 		counter = counter + 1
 		w1 = createWin(strategy, minRds, 0, counter)
 		wins.append(w1)
-		if minRds == 4 or minRds == 5:
+		#next if there are more than 4 roads in the game, include a win with 1 rb card
+		if minRds >= 4:
 			counter = counter + 1
-
 			w2 = createWin(strategy, minRds, 1, counter )
 			wins.append(w2) 
+		#last if there are more than 6 roads in the game, include a win with 2 rb cards
 		if minRds >= 6:
 			counter = counter + 1
 			w3 = createWin(strategy, minRds, 2, counter )
 			wins.append(w3)
+
 	return wins
 
 '''
-
+calculates the number and type of resource each different type of victory costs
 '''
 def calcCosts(winLt):
 	for i in winLt:
@@ -120,13 +162,15 @@ def calcCosts(winLt):
 
 	return winLt
 
-
+'''
+sort the list of wins from minimum cost to maximum
+'''
 def sortWins(winLt):
 	winLt.sort()
 	return winLt
 
 '''
-
+simple print function to help visualize each win
 '''
 def printWin(aWin):
 	print()
@@ -140,15 +184,22 @@ def printWin(aWin):
 	print("Resource Cards:\t", aWin.resourceDict)
 	print("Cost:\t\t", aWin.totalCost)
 
-'''
+	return True
 
 '''
-winList = createWinList()
-for i in winList:
-	print(i)
-print(len(winList))
-winList = calcCosts(winList)
-winList = sortWins(winList)
+main, create and complies all above functions into a usable win list
+'''
+def doEverything():
+	winList = createWinList()
+	print(len(winList))
+	winList = calcCosts(winList)
+	winList = sortWins(winList)
+
+	return winList
+
+
+
+winList = doEverything()
 for i in winList:
 	print(i)
 printWin(winList[0])
--- winType.py
+++ winType.py
@@ -8,21 +8,15 @@ class Win:
 
 	def __init__(self):
 
+		#I haven't included  monoplies and year of plenty since they are very variable
+		#and their use changes based on the iterations of the game
 		self.devCardDict = {
 			"knight": 0,
             "victoryPoint": 0,
             "roadBuilding": 0
 		}
 
-		self.numSettlements = 0
-		self.numCities = 0
-		self.largestArmy = False
-
-		self.longestRoad = False
-		self.minRoadsNeeded = 0
-
-		self.totalVicPts = 0
-
+		#amount of resources required to get that victory
 		self.resourceDict = {
 			"wheat": 0,
             "sheep": 0,
@@ -30,11 +24,28 @@ class Win:
             "ore": 0,
             "wood": 0
 		}
+
+		#number of Settlements and Cities required in that win
+		self.numSettlements = 0
+		self.numCities = 0
+
+		#bool based on whether or not a win required largest Arny
+		self.largestArmy = False
+
+		#bool based on whether or not win requires longest road as well as minimum roads
+		#needed to win
+		self.longestRoad = False
+		self.minRoadsNeeded = 0
+
+		#total victory points in this win
+		self.totalVicPts = 0
 		self.totalCost = 0
 
+		#a number to help keep track of this exact win
 		self.number = 0
 
 	'''
+	when you print the string you will get the number
 	'''
 	def __str__(self):
 		return  str(self.number)
@@ -44,31 +55,37 @@ class Win:
 	uses an input of similar dummy dictionaries and bools from the main AI function
 	'''
 	def setWinType(self, vicPts, army , road ,setts, cits):
+		#if army has 2 vic points, then the win requires largest army
 		if army == 2:
 			self.devCardDict["knight"] = 3
 			self.largestArmy = True
 
+		#if win has 2 vic points, then the win requires longest road
 		if road == 2:
 			self.longestRoad = True
 
+		#copies over the number of dev card victory points
 		self.devCardDict["victoryPoint"] = vicPts
 
+		#number of Settlements and cities in the win, cities divided by two, since they
+		#are worth double
 		self.numSettlements = setts
 		self.numCities = int(cits/2)
 
+		#set total victory points in this win
 		self.totalVicPts = vicPts + army + road +setts + cits
 
 	'''
-	
+	sets the information for roads
+	including minimum roads needed and how many road building cards were uesd
 	'''
 	def setRoads(self, minR, rBuilding):
 		self.minRoadsNeeded = minR
 		self.devCardDict["roadBuilding"] = rBuilding
 
 	'''
-
+	sets the arbitary counter , just to help access and debugging in the future
 	'''
-
 	def setNumber(self, counter):
 		self.number = counter
 
@@ -77,23 +94,33 @@ class Win:
 	returns the dictionary of the resources
 	'''	
 	def calcResourceCost(self):
+
+		#sum of all dev cards required in the win
 		devSum = self.devCardDict["knight"] + self.devCardDict["victoryPoint"] + self.devCardDict["roadBuilding"]
 
+		#adds neccesarry resources for all the dev cards
 		for i in range(0, devSum):
 			self.resourceDict["sheep"] = self.resourceDict["sheep"] + 1
 			self.resourceDict["ore"] = self.resourceDict["ore"] + 1
 			self.resourceDict["wheat"] = self.resourceDict["wheat"] + 1
-	
+		
+		#add the neccesarry resource for all the settlements plus the cities minus 2
+		#this is because all cities were settlements once
+		#also we start with 2 settlements that are free
 		for i in range(0, self.numSettlements + self.numCities - 2):
 			self.resourceDict["sheep"] = self.resourceDict["sheep"] + 1
 			self.resourceDict["wood"] = self.resourceDict["wood"] + 1
 			self.resourceDict["brick"] = self.resourceDict["brick"] + 1
 			self.resourceDict["wheat"] = self.resourceDict["wheat"] + 1
 
+		#add neccesarry resources for all the Cities
 		for i in range(0, self.numCities):
 			self.resourceDict["ore"] = self.resourceDict["ore"] + 3
 			self.resourceDict["wheat"] = self.resourceDict["wheat"] + 2
 
+		#add neccessary resources for all roads built
+		#minus road building since we accounted for that earlier
+		#minus the 2 roads we started with
 		for i in range(0, self.minRoadsNeeded - (2*self.devCardDict["roadBuilding"]) -2):
 			self.resourceDict["wood"] = self.resourceDict["wood"] + 1
 			self.resourceDict["brick"] = self.resourceDict["brick"] + 1
@@ -133,7 +160,8 @@ class Win:
 			return True
 		return False
 
-	'''	
+	'''
+	sets the less than symbol for comparisons and sorting	
 	'''
 	def __lt__(self, other):
 		return self.totalCost < other.totalCost
