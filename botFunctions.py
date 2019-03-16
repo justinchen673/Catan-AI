798afb9b85f88f5ac884f98f7fae86c944f1077d
justinchen673 <justinchenchen673@gmail.com>
2019-03-16 14:06:15 -0400

    added botfunctions

--- botFunctions.py
+++ botFunctions.py
@@ -1,3 +1,68 @@
+def calculateStrategyCost(bot, board):
+	rollProbabilities = {
+		2: 0.02778,
+		3: 0.05556,
+		4: 0.08336,
+		5: 0.11111,
+		6: 0.13889,
+		8: 0.13889,
+		9: 0.11111,
+		10: 0.08336,
+		11: 0.05556,
+		12: 0.02778
+	}
+
+	expectedResourcesPerRoll = {
+		"wheat": 0,
+		"sheep": 0,
+		"brick": 0,
+		"ore": 0,
+		"wood": 0
+	}
+
+	for i in range(0, 19):
+		currentHex = board.hexes[i]
+		if (currentHex.resourceType == "sand"):
+			continue
+		for j in board.hexRelationMatrix[i]:
+			if board.vertices[j].playerName == bot.name:
+				if (board.vertices[j].city):
+					expectedResourcesPerRoll[currentHex.resourceType] += rollProbabilities[currentHex.number] * 2
+				else:
+					expectedResourcesPerRoll[currentHex.resourceType] += rollProbabilities[currentHex.number]
+
+	print(expectedResourcesPerRoll)
+	rollsNeeded = {
+		"wheat": 0,
+		"sheep": 0,
+		"brick": 0,
+		"ore": 0,
+		"wood": 0
+	}
+	expectedResourceCount = {
+		"wheat": 0,
+		"sheep": 0,
+		"brick": 0,
+		"ore": 0,
+		"wood": 0
+	}
+	for win in bot.winList:
+		minRolls = float('inf')
+		for resource in rollsNeeded:
+			if (expectedResourcesPerRoll[resource] != 0):
+				rollsNeeded[resource] = win.resourceDict[resource] / expectedResourcesPerRoll[resource]
+				if (rollsNeeded[resource] < minRolls):
+					minRolls = rollsNeeded[resource]
+			else:
+				rollsNeeded[resource] = -1
+		for resource in expectedResourceCount:
+			expectedResourceNum = minRolls * expectedResourcesPerRoll[resource]
+		print(expectedResourceCount)
+
+
+
+	return
+
 def botHalveHand():
 	return
 
